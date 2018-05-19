f6971b4cf1b65eb77a5b7a5db989cfbc71a125d2
Marton Horvath <horvath.marton96@gmail.com>
2018-05-19 14:00:16 +0200

    Generate Material module for Material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { UsersComponent } from './users/users.component';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    UsersComponent
+    UsersComponent,
+    MaterialModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { MatTab } from '@angular/material';
+
+@NgModule({
+  imports: [MatTab],
+  exports: [MatTab]
+})
+export class MaterialModule { }
