47c3f977ef2d45c0ab65f1c40c9f17524e79feee
Marton Horvath <horvath.marton96@gmail.com>
2018-05-20 16:20:38 +0200

    Integrate SCSS to the project

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -9,7 +9,7 @@ import {PaginationInfo} from '../../models/PaginationInfo';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
--- src/app/admin/table/table.component.ts
+++ src/app/admin/table/table.component.ts
@@ -4,7 +4,7 @@ import {PageEvent} from '@angular/material';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
 
