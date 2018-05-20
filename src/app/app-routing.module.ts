cacb9b96323bc35691f0db446fb73d722eb422cd
Marton Horvath <horvath.marton96@gmail.com>
2018-05-20 14:04:57 +0200

    Implement route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import {ProductsComponent} from './admin/products/products.component';
-import {UsersComponent} from './admin/users/users.component';
-import {LoginComponent} from './admin/login/login.component';
-import {UserListComponent} from './user/user-list/user-list.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { UsersComponent } from './admin/users/users.component';
+import { LoginComponent } from './admin/login/login.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+
+import {AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: UserListComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuardService] },
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuardService] }
 ];
 
 @NgModule({
